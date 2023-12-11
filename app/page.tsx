28d9f5521ef9c53b7a48cd2c5da06be99b27460b
artmadeit <this.arthur.email@gmail.com>
2023-12-10 22:32:09 -0500

    feat: add more fields and date picker

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
 "use client";
 import { Button } from "@mui/material";
-import { FormContainer, TextFieldElement } from "react-hook-form-mui";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  DatePickerElement,
+  FormContainer,
+  TextFieldElement,
+} from "react-hook-form-mui";
+
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 export default function Home() {
   return (
-    <FormContainer
-      defaultValues={{ name: "" }}
-      onSuccess={(data) => console.log(data)}
-    >
-      <TextFieldElement name="name" label="Name" required />
-      <Button type="submit">Submit</Button>
-    </FormContainer>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <FormContainer
+        defaultValues={{
+          firstName: "",
+          lastName: "",
+          age: null,
+          birthdate: null,
+        }}
+        onSuccess={(data) => console.log(data)}
+      >
+        <TextFieldElement name="firstName" label="Nombre" required />
+        <TextFieldElement name="lastName" label="Apellido" />
+        {/* TODO: telefono */}
+        <DatePickerElement label="Año de nacimiento" name="birthdate" />
+        <TextFieldElement name="age" label="Edad" type="number" />
+        <Button type="submit">Submit</Button>
+      </FormContainer>
+    </LocalizationProvider>
   );
 }
