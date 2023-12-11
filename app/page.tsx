5c568727f5def4269ab5d9993e90e49c17b7b6c8
artmadeit <this.arthur.email@gmail.com>
2023-12-10 23:02:06 -0500

    feat: mui tel input

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ import {
   DatePickerElement,
   FormContainer,
   TextFieldElement,
+  useFormContext,
 } from "react-hook-form-mui";
 
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { MuiTelInput } from "mui-tel-input";
 
 export default function Home() {
   return (
@@ -16,6 +18,7 @@ export default function Home() {
         defaultValues={{
           firstName: "",
           lastName: "",
+          phoneNumber: "+51",
           age: null,
           birthdate: null,
         }}
@@ -23,11 +26,39 @@ export default function Home() {
       >
         <TextFieldElement name="firstName" label="Nombre" required />
         <TextFieldElement name="lastName" label="Apellido" />
-        {/* TODO: telefono */}
-        <DatePickerElement label="Año de nacimiento" name="birthdate" />
+        <TelFieldElement name="phoneNumber" />
+        <DatePickerElement label="Fecha de nacimiento" name="birthdate" />
         <TextFieldElement name="age" label="Edad" type="number" />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" variant="contained">
+          Guardar
+        </Button>
       </FormContainer>
     </LocalizationProvider>
   );
 }
+
+const TelFieldElement = ({ name }: { name: string }) => {
+  const { setValue, watch } = useFormContext();
+
+  const phoneNumber = watch(name);
+
+  return (
+    <MuiTelInput
+      value={phoneNumber}
+      onChange={(value) => setValue("phoneNumber", value.replace(/\s+/g, ""))}
+      // TODO: error and helper
+      // error={
+      // 	touched.phoneNumber &&
+      // 	Boolean(errors.phoneNumber)
+      // }
+      // helperText={
+      // 	(touched.phoneNumber &&
+      // 		errors.phoneNumber) ||
+      // 	""
+      // }
+      fullWidth
+      label="Teléfono"
+      onlyCountries={["PE"]}
+    />
+  );
+};
