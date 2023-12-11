5fe507d5bf43a9a2e80f4a4bbc55b662ad5c3819
artmadeit <this.arthur.email@gmail.com>
2023-12-11 16:51:42 -0500

    feat(event): list idea

--- app/portal/event/page.tsx
+++ app/portal/event/page.tsx
@@ -1,13 +1,74 @@
+"use client";
 import { Fab, Stack, Tooltip, Typography } from "@mui/material";
 import Link from "next/link";
 
 import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  esES,
+} from "@mui/x-data-grid";
+import { useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { withOutSorting } from "@/app/(components)/helpers/withOutSorting";
+import { usePagination } from "@/app/(components)/hook-customization/usePagination";
+import { Page } from "@/app/(api)/pagination";
+
+type EventListItem = {
+  id: number;
+  name: string;
+};
 
 export default function EventListPage() {
+  const router = useRouter();
+  const { paginationModel, setPaginationModel } = usePagination();
+
+  const isLoading = false;
+  const events: Page<EventListItem> = {
+    _embedded: {
+      events: [],
+    },
+    _links: {},
+    page: {
+      size: 1,
+      totalElements: 0,
+      totalPages: 1,
+      number: 1,
+    },
+  };
+
+  const columns = useMemo(
+    () =>
+      (
+        [
+          { field: "name", headerName: "Nombre" },
+          {
+            field: "actions",
+            type: "actions",
+            width: 80,
+            getActions: (params) => {
+              return [
+                <Tooltip title="Ver" key="edit">
+                  <GridActionsCellItem
+                    icon={<SearchIcon />}
+                    label="ver"
+                    onClick={() => router.push(`events/${params.row.id}`)}
+                  />
+                </Tooltip>,
+              ];
+            },
+          },
+        ] as GridColDef<EventListItem>[]
+      ).map(withOutSorting),
+    [router]
+  );
+
   return (
     <Stack direction="column" spacing={2}>
       <Stack direction="row" alignItems="center" spacing={2}>
-        <Typography variant="h4">Lista de eventos</Typography>
+        <Typography variant="h4">Eventos</Typography>
         <Tooltip title="Registrar">
           <Link href="event/create">
             <Fab color="primary" aria-labelledby="add">
@@ -30,6 +91,19 @@ export default function EventListPage() {
         }}
         fullWidth
       /> */}
+      <div style={{ height: "70vh", width: "100%" }}>
+        <DataGrid
+          loading={isLoading}
+          columns={columns}
+          rowCount={events?.page.totalElements || 0}
+          paginationModel={paginationModel}
+          paginationMode="server"
+          onPaginationModelChange={setPaginationModel}
+          disableColumnFilter
+          rows={events?._embedded.patients || []}
+          localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+        />
+      </div>
     </Stack>
   );
 }
