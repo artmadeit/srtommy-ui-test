6d74de8a2102cf31fbb503a36a14ac4292eaa01e
artmadeit <this.arthur.email@gmail.com>
2023-12-12 11:20:25 -0500

    feat(event): create idea

--- app/portal/event/create/page.tsx
+++ app/portal/event/create/page.tsx
@@ -1,3 +1,129 @@
+"use client";
+import { Button, Typography } from "@mui/material";
+import {
+  Controller,
+  DatePickerElement,
+  FormContainer,
+  TextFieldElement,
+  TimePickerElement,
+  useFormContext,
+} from "react-hook-form-mui";
+
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { MuiTelInput } from "mui-tel-input";
+
 export default function EventCreatePage() {
-  return <div>lalala</div>;
+  return (
+    <FormContainer
+      defaultValues={{
+        name: "",
+        startDate: null,
+        startTime: null,
+        endDate: null,
+        endTime: null,
+        place: "",
+        description: "",
+        speaker: "",
+      }}
+      onSuccess={(data) => console.log(data)}
+    >
+      <Grid container spacing={2} padding={2}>
+        <Grid xs={12}>
+          <TextFieldElement fullWidth name="name" label="Nombre" required />
+        </Grid>
+        <Grid xs={3}>
+          <DatePickerElement
+            sx={{ width: "100%" }}
+            label="Fecha inicio"
+            name="startDate"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <TimePickerElement
+            sx={{ width: "100%" }}
+            label="Hora inicio"
+            name="startTime"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <DatePickerElement
+            sx={{ width: "100%" }}
+            label="Fecha fin"
+            name="endDate"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <TimePickerElement
+            sx={{ width: "100%" }}
+            label="Hora fin"
+            name="endTime"
+            required
+          />
+        </Grid>
+        <Grid xs={12}>
+          <TextFieldElement fullWidth name="place" label="Lugar" />
+        </Grid>
+        <Grid xs={12}>
+          <TextFieldElement fullWidth name="speaker" label="Speaker" />
+        </Grid>
+        <Grid xs={12}>
+          <TextFieldElement
+            fullWidth
+            name="description"
+            label="Descripción"
+            multiline
+            minRows={4}
+          />
+        </Grid>
+        <Grid xs={6}>
+          <Button type="submit" variant="contained">
+            Guardar
+          </Button>
+        </Grid>
+      </Grid>
+    </FormContainer>
+  );
 }
+
+const TelFieldElement = ({ name }: { name: string }) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      // rules={{ required: true }}
+      render={({ field }) => (
+        <MuiTelInput
+          name={field.name}
+          value={field.value}
+          onChange={(value) => {
+            field.onChange(value.replace(/\s+/g, ""));
+          }}
+          // required
+          // TODO: error and helper
+          error={
+            Boolean(errors[name])
+            // touched.phoneNumber &&
+            // Boolean(errors.phoneNumber)
+          }
+          // helperText={
+          //   (errors[name]?.message as string) || ""
+          //   // (touched.phoneNumber &&
+          //   // 	errors.phoneNumber) ||
+          //   // ""
+          // }
+          fullWidth
+          label="Teléfono"
+          onlyCountries={["PE"]}
+        />
+      )}
+    />
+  );
+};
