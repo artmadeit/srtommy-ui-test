f1c04254684459993c64be0163554ce4d218ead8
Dede1421 <mauricioandre91@gmail.com>
2023-12-18 13:02:13 -0500

    feat(organization): add a form draft

--- app/portal/organization/page.tsx
+++ app/portal/organization/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+
+import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { FormContainer, TextFieldElement } from "react-hook-form-mui";
 
 export default function Organization() {
   return (
-    <div>
-      <h1>TO DO</h1>
-    </div>
+    <FormContainer
+      defaultValues={{
+        name: "",
+        address: "",
+      }}
+      onSuccess={(x) => console.log(x)}
+    >
+      <Grid container spacing={2} padding={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5" gutterBottom>
+            Datos generales de la organización
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextFieldElement name="name" label="Nombre" required fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextFieldElement
+            name="address"
+            label="Dirección"
+            required
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          Hola
+        </Grid>
+      </Grid>
+    </FormContainer>
   );
 }
