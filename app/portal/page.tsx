607791f96cc15d6cac2d04a12001ec639728cd3a
artmadeit <this.arthur.email@gmail.com>
2023-12-30 16:46:55 -0500

    feat: introduce favicon and portal home

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -1,9 +1,43 @@
+import { Grid, List, ListItem, Typography } from "@mui/material";
+import Image from "next/image";
 import MenuDrawer2 from "./MenuDrawer2";
+import tommyImage from "./tomy.png";
 
 export default function PortalHome() {
   return (
     <main>
-      <MenuDrawer2>TODO</MenuDrawer2>
+      <MenuDrawer2
+        listDrawer={
+          <List>
+            <ListItem>
+              {/* TODO: randomize versicles?*/}
+              <blockquote>
+                Mejores son dos que uno; porque tienen mejor paga de su trabajo.
+                Porque si caen, el uno levantará a su compañero; pero ¡ay del
+                solo! que cuando caiga, no habrá segundo que lo levante.
+                <br />
+                <br />
+                <em>Eclesiastés 4:9-10 (Reina-Valera 1960)</em>
+              </blockquote>
+            </ListItem>
+          </List>
+        }
+      >
+        <Grid container>
+          <Grid item xs={4}>
+            <Image src={tommyImage} alt="Picture of the author" />
+          </Grid>
+          <Grid item xs={6} pt={4}>
+            <Typography variant="h3" gutterBottom>
+              Bienvenido
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              Soy el sr. Tommy, sere su fiel ayudante, cree o seleccione una
+              sede para empezar a administrar su iglesia
+            </Typography>
+          </Grid>
+        </Grid>
+      </MenuDrawer2>
     </main>
   );
 }
