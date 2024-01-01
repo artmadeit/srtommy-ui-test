a0d678450d6bbc3a09adbff5397ff922f43a5b07
artmadeit <this.arthur.email@gmail.com>
2024-01-01 10:43:44 -0500

    feat: add builder

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,17 +1,22 @@
-import { RenderBuilderContent } from "@/components/builder";
 import { builder } from "@builder.io/sdk";
-import Link from "next/link";
+import { RenderBuilderContent } from "../../components/builder";
 
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-export default async function Home() {
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: PageProps) {
   const content = await builder
     // Get the page content from Builder with the specified options
     .get("page", {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
-        urlPath: "/",
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
       },
     })
     // Convert the result to a promise
