ba34825e4774d695dcee0a2b50e765b4197de550
artmadeit <this.arthur.email@gmail.com>
2024-01-05 12:34:32 -0500

    feat: add tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>{children}</body>
-      {/*TODO: daniel <GoogleTagManager gtmId="GTM-XYZ" /> */}
+      <GoogleTagManager gtmId="GTM-KHZ22MM9" />
     </html>
   );
 }
