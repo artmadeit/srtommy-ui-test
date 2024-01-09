5555dc2cc63561b8c4e997f931c55cec3d946d30
Dede1421 <mauricioandre91@gmail.com>
2024-01-09 17:28:25 -0500

    feat: create an EventForm component

---
+++ app/(components)/EventForm.tsx
@@ -0,0 +1,161 @@
+import {
+  AutocompleteElement,
+  DatePickerElement,
+  FormContainer,
+  SubmitHandler,
+  TextFieldElement,
+  TimePickerElement,
+  UseFormReturn,
+  useForm,
+} from "react-hook-form-mui";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Button, Typography } from "@mui/material";
+import { useAuthApi } from "../(api)/api";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useDebounce } from "use-debounce";
+import { DEBOUNCE_WAIT_MS } from "./helpers/debouncing";
+import { SnackbarContext } from "./SnackbarContext";
+import { OrganizationDetail } from "../portal/[orgId]/Organization";
+import { SpringPage } from "../(api)/pagination";
+import useSWR from "swr";
+import { PersonDetail } from "../portal/[orgId]/person/Person";
+
+type Option = {
+  id: number;
+  label: string;
+};
+
+type Event = {
+  name: string;
+  startDate: Date;
+  startTime: Date;
+  endDate: Date;
+  endTime: Date;
+  address: string;
+  description: string;
+  speakers: Option[];
+};
+
+type EventFormProps = {
+  orgId: number;
+  initialValues: any;
+  submit: SubmitHandler<Event>;
+};
+
+export const EventForm = ({ orgId, initialValues, submit }: EventFormProps) => {
+  const [searchTextSpeaker, setSearchTextSpeaker] = React.useState("");
+  const getApi = useAuthApi();
+  const router = useRouter();
+  const alert = React.useContext(SnackbarContext);
+
+  const formContext = useForm<Event>({
+    defaultValues: initialValues,
+  });
+
+  const [searchTextDebounced] = useDebounce(
+    searchTextSpeaker,
+    DEBOUNCE_WAIT_MS
+  );
+
+  const { data: organization } = useSWR<OrganizationDetail>(
+    `organizations/${orgId}`
+  );
+
+  const { data: people } = useSWR<SpringPage<PersonDetail>>(
+    searchTextDebounced ? `people?searchText=${searchTextDebounced}` : `people`
+  );
+
+  return (
+    <FormContainer formContext={formContext} onSuccess={submit}>
+      <Grid container spacing={2} padding={2}>
+        <Grid xs={12}>
+          <Typography variant="h5" gutterBottom>
+            Datos del Evento
+          </Typography>
+        </Grid>
+        <Grid xs={12}>
+          <TextFieldElement fullWidth name="name" label="Nombre" required />
+        </Grid>
+        <Grid xs={3}>
+          <DatePickerElement
+            sx={{ width: "100%" }}
+            label="Fecha inicio"
+            name="startDate"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <TimePickerElement
+            sx={{ width: "100%" }}
+            label="Hora inicio"
+            name="startTime"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <DatePickerElement
+            sx={{ width: "100%" }}
+            label="Fecha fin"
+            name="endDate"
+            required
+          />
+        </Grid>
+        <Grid xs={3}>
+          <TimePickerElement
+            sx={{ width: "100%" }}
+            label="Hora fin"
+            name="endTime"
+            required
+          />
+        </Grid>
+        <Grid xs={12}>
+          <AutocompleteElement
+            autocompleteProps={{
+              freeSolo: true,
+              onInputChange: (_event, newInputValue) => {
+                formContext.setValue("address", newInputValue);
+              },
+            }}
+            name="address"
+            label="Lugar"
+            options={[organization?.address]}
+          />
+        </Grid>
+        <Grid xs={12}>
+          {/* TODO: make creatable */}
+          <AutocompleteElement
+            multiple
+            autocompleteProps={{
+              onInputChange: (_event, newInputValue) => {
+                setSearchTextSpeaker(newInputValue);
+              },
+            }}
+            name="speakers"
+            label="Ponente(s)"
+            options={
+              people?.content.map((x) => ({
+                id: x.id,
+                label: x.firstName + " " + x.lastName,
+              })) || []
+            }
+          />
+        </Grid>
+        <Grid xs={12}>
+          <TextFieldElement
+            fullWidth
+            name="description"
+            label="Descripción"
+            multiline
+            minRows={4}
+          />
+        </Grid>
+        <Grid xs={6}>
+          <Button type="submit" variant="contained">
+            Guardar
+          </Button>
+        </Grid>
+      </Grid>
+    </FormContainer>
+  );
+};
--- app/portal/[orgId]/event/create/page.tsx
+++ app/portal/[orgId]/event/create/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import {
   AutocompleteElement,
   DatePickerElement,
@@ -10,31 +10,17 @@ import {
 } from "react-hook-form-mui";
 
 import { SpringPage } from "@/app/(api)/pagination";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
+
 import React, { useState } from "react";
 import useSWR from "swr";
 import { OrganizationDetail } from "../../Organization";
-import { useDebounce } from "use-debounce";
+
 import { DEBOUNCE_WAIT_MS } from "@/app/(components)/helpers/debouncing";
 import { PersonDetail } from "../../person/Person";
 import { useAuthApi } from "@/app/(api)/api";
 import { useRouter } from "next/navigation";
 import { SnackbarContext } from "@/app/(components)/SnackbarContext";
-type Option = {
-  id: number;
-  label: string;
-};
-
-type Event = {
-  name: string;
-  startDate: Date;
-  startTime: Date;
-  endDate: Date;
-  endTime: Date;
-  address: string;
-  description: string;
-  speakers: Option[];
-};
+import { EventForm } from "@/app/(components)/EventForm";
 
 export default function EventCreatePage({
   params,
@@ -42,34 +28,10 @@ export default function EventCreatePage({
   params: { orgId: number };
 }) {
   const { orgId } = params;
-  const formContext = useForm<Event>({
-    defaultValues: {
-      name: "",
-      // startDate: null,
-      // startTime: null,
-      // endDate: null,
-      // endTime: null,
-      address: "",
-      description: "",
-      speakers: [],
-    },
-  });
 
-  const [searchTextSpeaker, setSearchTextSpeaker] = useState("");
   const getApi = useAuthApi();
   const router = useRouter();
   const alert = React.useContext(SnackbarContext);
-  const { data: organization } = useSWR<OrganizationDetail>(
-    `organizations/${orgId}`
-  );
-
-  const [searchTextDebounced] = useDebounce(
-    searchTextSpeaker,
-    DEBOUNCE_WAIT_MS
-  );
-  const { data: people } = useSWR<SpringPage<PersonDetail>>(
-    searchTextDebounced ? `people?searchText=${searchTextDebounced}` : `people`
-  );
 
   function getDateTime(date: Date, time: Date) {
     return `${date.toISOString().split("T")[0]}T${
@@ -78,118 +40,41 @@ export default function EventCreatePage({
   }
 
   return (
-    <FormContainer
-      formContext={formContext}
-      onSuccess={async (values) => {
-        // const { startDate, endDate, startTime, endTime,
-        //    ...rest} = values;
-        const data = {
-          name: values.name,
-          address: values.address,
-          organizationId: orgId,
-          startTime: getDateTime(values.startDate, values.startTime),
-          endTime: getDateTime(values.endDate, values.endTime),
-          speakerIds: values.speakers.map((x) => x.id),
-          description: values.description,
-        };
+    <Box>
+      <EventForm
+        orgId={orgId}
+        initialValues={{
+          name: "",
+          // startDate: null,
+          // startTime: null,
+          // endDate: null,
+          // endTime: null,
+          address: "",
+          description: "",
+          speakers: [],
+        }}
+        submit={async (values) => {
+          // const { startDate, endDate, startTime, endTime,
+          //    ...rest} = values;
+          const data = {
+            name: values.name,
+            address: values.address,
+            organizationId: orgId,
+            startTime: getDateTime(values.startDate, values.startTime),
+            endTime: getDateTime(values.endDate, values.endTime),
+            speakerIds: values.speakers.map((x) => x.id),
+            description: values.description,
+          };
 
-        const response = await getApi().then((api) =>
-          api.post(`/events`, data)
-        );
-        alert.showMessage("Evento registrado exitosamente");
-        router.push(`/portal/${orgId}/event`);
-        // console.log(values.speakers);
-        // console.log(data)
-      }}
-    >
-      <Grid container spacing={2} padding={2}>
-        <Grid xs={12}>
-          <Typography variant="h5" gutterBottom>
-            Datos del Evento
-          </Typography>
-        </Grid>
-        <Grid xs={12}>
-          <TextFieldElement fullWidth name="name" label="Nombre" required />
-        </Grid>
-        <Grid xs={3}>
-          <DatePickerElement
-            sx={{ width: "100%" }}
-            label="Fecha inicio"
-            name="startDate"
-            required
-          />
-        </Grid>
-        <Grid xs={3}>
-          <TimePickerElement
-            sx={{ width: "100%" }}
-            label="Hora inicio"
-            name="startTime"
-            required
-          />
-        </Grid>
-        <Grid xs={3}>
-          <DatePickerElement
-            sx={{ width: "100%" }}
-            label="Fecha fin"
-            name="endDate"
-            required
-          />
-        </Grid>
-        <Grid xs={3}>
-          <TimePickerElement
-            sx={{ width: "100%" }}
-            label="Hora fin"
-            name="endTime"
-            required
-          />
-        </Grid>
-        <Grid xs={12}>
-          <AutocompleteElement
-            autocompleteProps={{
-              freeSolo: true,
-              onInputChange: (_event, newInputValue) => {
-                formContext.setValue("address", newInputValue);
-              },
-            }}
-            name="address"
-            label="Lugar"
-            options={[organization?.address]}
-          />
-        </Grid>
-        <Grid xs={12}>
-          {/* TODO: make creatable */}
-          <AutocompleteElement
-            multiple
-            autocompleteProps={{
-              onInputChange: (_event, newInputValue) => {
-                setSearchTextSpeaker(newInputValue);
-              },
-            }}
-            name="speakers"
-            label="Ponente(s)"
-            options={
-              people?.content.map((x) => ({
-                id: x.id,
-                label: x.firstName + " " + x.lastName,
-              })) || []
-            }
-          />
-        </Grid>
-        <Grid xs={12}>
-          <TextFieldElement
-            fullWidth
-            name="description"
-            label="Descripción"
-            multiline
-            minRows={4}
-          />
-        </Grid>
-        <Grid xs={6}>
-          <Button type="submit" variant="contained">
-            Guardar
-          </Button>
-        </Grid>
-      </Grid>
-    </FormContainer>
+          const response = await getApi().then((api) =>
+            api.post(`/events`, data)
+          );
+          alert.showMessage("Evento registrado exitosamente");
+          router.push(`/portal/${orgId}/event`);
+          // console.log(values.speakers);
+          // console.log(data)
+        }}
+      />
+    </Box>
   );
 }
