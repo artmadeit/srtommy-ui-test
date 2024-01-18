f7ffefd1f302119ac78e1cc5d750f968e67638c8
Dede1421 <mauricioandre91@gmail.com>
2024-01-18 11:56:28 -0500

    feat(organization): add integration for edit

--- app/portal/[orgId]/organization/page.tsx
+++ app/portal/[orgId]/organization/page.tsx
@@ -5,8 +5,10 @@ import React from "react";
 import { OrganizationForm } from "../../../(components)/OrganizationForm";
 import useSWR from "swr";
 import { OrganizationDetail } from "../Organization";
-
 import Loading from "@/app/(components)/Loading";
+import { useAuthApi } from "@/app/(api)/api";
+import { SnackbarContext } from "@/app/(components)/SnackbarContext";
+// import { useRouter } from "next/navigation";
 
 export default function Organization({
   params,
@@ -19,6 +21,10 @@ export default function Organization({
     `/organizations/${orgId}`
   );
 
+  const getApi = useAuthApi();
+  const alert = React.useContext(SnackbarContext);
+  // const router = useRouter();
+
   if (isLoading) return <Loading />;
 
   return (
@@ -29,7 +35,16 @@ export default function Organization({
         <OrganizationForm
           initialValues={organization}
           title="Datos generales de la organización"
-          submit={async (values) => console.log(values)}
+          submit={async (formValues) => {
+            const api = await getApi();
+            await api.put(`/organizations/${orgId}`, {
+              name: formValues.name,
+              address: formValues.address,
+              phoneNumber: formValues.phoneNumber,
+            });
+            alert.showMessage("Se editó");
+            // router.push("");
+          }}
         />
       )}
     </Box>
