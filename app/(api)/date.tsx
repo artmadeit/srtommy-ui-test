3f95b5f3c8c74887939c914fd5e4dea9538e8333
artmadeit <this.arthur.email@gmail.com>
2024-01-21 14:06:09 -0500

    fix: for zoned datetime

--- app/(api)/date.tsx
+++ app/(api)/date.tsx
@@ -12,8 +12,15 @@ export const parseDate = (date: string) =>
 
 const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?$/;
 
+// see: https://stackoverflow.com/questions/28020805/regex-validate-correct-iso8601-date-string-with-time
+const isoZonedDateFormat = /^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)$/
+
 function isIsoDateString(value: any): boolean {
   return value && typeof value === "string" && isoDateFormat.test(value);
+}
+
+function isIsoZonedDateString(value: any): boolean {
+  return value && typeof value === "string" && isoZonedDateFormat.test(value);
 }
 
 // See: https://stackoverflow.com/questions/65692061/casting-dates-properly-from-an-api-response-in-typescript
@@ -24,6 +31,7 @@ export function handleDates(body: any) {
   for (const key of Object.keys(body)) {
     const value = body[key];
     if (isIsoDateString(value)) body[key] = parseISO(value);
+    else if(isIsoZonedDateString(value)) body[key] = new Date(value);
     else if (isValid(parseDate(value))) body[key] = parseDate(value);
     else if (typeof value === "object") handleDates(value);
   }
