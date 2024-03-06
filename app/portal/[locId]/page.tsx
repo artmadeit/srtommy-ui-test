2aee8e5a6ed9e94afd1240f6faa792530493c7b3
Dede1421 <mauricioandre91@gmail.com>
2024-03-06 09:41:28 -0500

    feat: test streamlit

--- app/portal/[locId]/page.tsx
+++ app/portal/[locId]/page.tsx
@@ -1,3 +1,12 @@
 export default function PortalHome() {
-  return <main>TODO id</main>;
+  return (
+    <main>
+      TODO
+      <iframe
+        src="https://dashboard-srtommy.streamlit.app/?embed=true"
+        height="450"
+        style={{ width: "100%", border: "none" }}
+      ></iframe>
+    </main>
+  );
 }
