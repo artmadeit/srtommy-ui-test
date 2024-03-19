8cb49cf4016e0f721e4986478fd4d0912e094d70
Dede1421 <mauricioandre91@gmail.com>
2024-03-19 14:40:16 -0500

    feat(courses): add a DataGrid

--- app/portal/[locId]/courses/page.tsx
+++ app/portal/[locId]/courses/page.tsx
@@ -1,8 +1,54 @@
+"use client";
+
 import { Fab, Stack, Tooltip, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Link from "next/link";
+import { DataGrid, GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
+import { useMemo } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { useRouter } from "next/navigation";
+import { withOutSorting } from "@/app/(components)/helpers/withOutSorting";
+import useSWR from "swr";
+import { SpringPage } from "@/app/(api)/pagination";
+
+type CourseLisItem = {
+  id: number;
+  name: string;
+};
 
 export default function CourseListPage() {
+  const router = useRouter();
+  const { data: events2, isLoading } =
+    useSWR<SpringPage<CourseLisItem>>("/events");
+
+  const columns = useMemo(
+    () =>
+      (
+        [
+          { field: "name", headerName: "Nombre" },
+          {
+            field: "actions",
+            type: "actions",
+            width: 80,
+            getActions: (params) => {
+              return [
+                <Tooltip title="Ver" key="see">
+                  <GridActionsCellItem
+                    icon={<SearchIcon />}
+                    label="Ver"
+                    onClick={() =>
+                      router.push(`event/${params.row.id}/attendance`)
+                    }
+                  />
+                </Tooltip>,
+              ];
+            },
+          },
+        ] as GridColDef<CourseLisItem>[]
+      ).map(withOutSorting),
+    [router]
+  );
+
   return (
     <Stack direction="column" spacing={2} p={4}>
       <Stack direction="row" alignItems="center" spacing={2}>
@@ -15,6 +61,13 @@ export default function CourseListPage() {
           </Link>
         </Tooltip>
       </Stack>
+      <div style={{ height: "70vh", width: "100%" }}>
+        <DataGrid
+          loading={isLoading}
+          columns={columns}
+          rows={events2?.content || []}
+        />
+      </div>
     </Stack>
   );
 }
