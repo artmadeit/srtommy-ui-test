d2772386b4914bb03b45b9c4f305cac00edaccc4
Dede1421 <mauricioandre91@gmail.com>
2024-03-19 11:44:13 -0500

    feat(courses): add fab icon

--- app/portal/[locId]/courses/page.tsx
+++ app/portal/[locId]/courses/page.tsx
@@ -1,9 +1,20 @@
-import { Typography } from "@mui/material";
+import { Fab, Stack, Tooltip, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Link from "next/link";
 
 export default function CourseListPage() {
   return (
-    <div>
-      <Typography variant="h4">Cursos</Typography>
-    </div>
+    <Stack direction="column" spacing={2} p={4}>
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <Typography variant="h4">Cursos</Typography>
+        <Tooltip title="Registrar">
+          <Link href="/portal/">
+            <Fab color="primary" aria-labelledby="add">
+              <AddIcon />
+            </Fab>
+          </Link>
+        </Tooltip>
+      </Stack>
+    </Stack>
   );
 }
