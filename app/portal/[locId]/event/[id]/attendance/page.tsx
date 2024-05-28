e68803e68d739450a5dfd67967ddf7c4cd9f35f5
Dede1421 <mauricioandre91@gmail.com>
2024-05-28 14:51:46 -0500

    feat: add a DialogDelete

--- app/portal/[locId]/event/[id]/attendance/page.tsx
+++ app/portal/[locId]/event/[id]/attendance/page.tsx
@@ -2,10 +2,21 @@
 
 import { useAuthApi } from "@/app/(api)/api";
 import Loading from "@/app/(components)/Loading";
-import { Box, Button, Fab, Stack, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Fab,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { GridRowSelectionModel } from "@mui/x-data-grid";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import useSWR from "swr";
 import { PersonTable, fullName } from "../../../person/PersonTable";
 import { EventDetail } from "../EventDetail";
@@ -33,6 +44,8 @@ export default function AttendanceEvent({
   const { data: eventAttendance, isLoading } = useSWR<EventAttendance>(
     `/events/${id}/attendance`
   );
+
+  const [openD, setOpenD] = React.useState(false);
 
   const [rowSelectionModel, setRowSelectionModel] =
     useState<GridRowSelectionModel>([]);
@@ -72,7 +85,10 @@ export default function AttendanceEvent({
             <Box>
               <div style={{ display: "flex", justifyContent: "flex-end" }}>
                 <Tooltip title="Eliminar">
-                  <Fab aria-labelledby="delete">
+                  <Fab
+                    aria-labelledby="delete"
+                    onClick={() => setOpenD(true)}
+                  >
                     <DeleteIcon />
                   </Fab>
                 </Tooltip>
@@ -84,6 +100,7 @@ export default function AttendanceEvent({
                 editable={false}
               />
             </Box>
+            <DialogDelete open={openD} close={() => setOpenD(false)} />
           </Grid>
           <Grid xs={12}>
             <Typography variant="h6" gutterBottom>
@@ -125,3 +142,21 @@ export default function AttendanceEvent({
     </Grid>
   );
 }
+
+type DialogDeleteProps = {
+  close: () => void;
+  open: boolean;
+};
+
+const DialogDelete = ({ close, open }: DialogDeleteProps) => {
+  return (
+    <Dialog open={open} onClose={close}>
+      <DialogTitle>Borrar el evento recurrente</DialogTitle>
+      <DialogContent>ABCCD</DialogContent>
+      <DialogActions>
+        <Button onClick={close}>Cancelar</Button>
+        <Button>Aceptar</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
