010d73fd16a75a2ffb096198213310d64ef9b3aa
Dede1421 <mauricioandre91@gmail.com>
2024-06-10 16:05:48 -0500

    feat: add an input in profilePage

--- app/portal/[locId]/profile/page.tsx
+++ app/portal/[locId]/profile/page.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { Box, Grid, Stack, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { FormContainer, TextFieldElement } from "react-hook-form-mui";
 
 export default function ProfilePage() {
   return (
     <FormContainer>
-      <Grid container p={4}>
+      <Grid container spacing={2} margin={4}>
         <Grid xs={12}>
-          <Typography variant="h4">Perfil</Typography>
+          <Typography variant="h5" gutterBottom>
+            Perfil
+          </Typography>
         </Grid>
-        <Grid>
-          <TextFieldElement
-            name="email"
-            label="Email"
-          />
+        <Grid xs={6}>
+          <TextFieldElement name="email" label="Email" type="email" fullWidth />
+        </Grid>
+        <Grid xs={12}>
+          <Typography variant="h5">Pagar en: </Typography>
         </Grid>
       </Grid>
     </FormContainer>
