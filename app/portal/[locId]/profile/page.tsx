d0a4532cc03b7c1e4ad3380d32dc7bd8ef3480c2
Dede1421 <mauricioandre91@gmail.com>
2024-06-10 10:30:12 -0500

    feat: add a draft form in profile

--- app/portal/[locId]/profile/page.tsx
+++ app/portal/[locId]/profile/page.tsx
@@ -1,11 +1,22 @@
-import { Box, Stack, Typography } from "@mui/material";
+"use client";
+
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import { FormContainer, TextFieldElement } from "react-hook-form-mui";
 
 export default function ProfilePage() {
   return (
-    <Stack direction="column" spacing={2} p={4}>
-      <Stack>
-        <Typography variant="h4">Perfil</Typography>
-      </Stack>
-    </Stack>
+    <FormContainer>
+      <Grid container p={4}>
+        <Grid xs={12}>
+          <Typography variant="h4">Perfil</Typography>
+        </Grid>
+        <Grid>
+          <TextFieldElement
+            name="email"
+            label="Email"
+          />
+        </Grid>
+      </Grid>
+    </FormContainer>
   );
 }
